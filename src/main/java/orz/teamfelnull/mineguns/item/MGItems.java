2c4283f2d64ea528268cd39a46e885fff48bc431
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2020-01-18 01:18:31 +0900

    19419

--- src/main/java/orz/teamfelnull/mineguns/item/MGItems.java
+++ src/main/java/orz/teamfelnull/mineguns/item/MGItems.java
@@ -9,13 +9,17 @@ import orz.teamfelnull.mineguns.gun.MGGuns;
 public class MGItems {
 	public static Item TEST_ITEM = new Item(new Item.Properties().group(MGItemGroup.MINEGUN_TAB))
 			.setRegistryName(MineGuns.MODID, "test_item");
+
+
 	public static Item KYOMUH = new Item(new Item.Properties().group(MGItemGroup.MINEGUN_TAB))
 			.setRegistryName(MineGuns.MODID, "kyomu");
+
+
 	public static Item TEST_GUN = new GunItem(new Item.Properties().group(MGItemGroup.MINEGUN_TAB), MGGuns.TEST)
 			.setRegistryName(MineGuns.MODID, "test_gun");
 
 	public static void registerItem(IForgeRegistry<Item> r) {
-
+//test
 		r.register(MGItems.TEST_ITEM);
 		r.register(MGItems.TEST_GUN);
 		r.register(MGItems.KYOMUH);
