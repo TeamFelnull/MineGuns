6bbccc854830bc1f3fa4bc6e99715990ac06fe7d
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2020-01-17 18:16:57 +0900

    ンアー

--- src/main/java/orz/teamfelnull/mineguns/MineGuns.java
+++ src/main/java/orz/teamfelnull/mineguns/MineGuns.java
@@ -26,7 +26,7 @@ public class MineGuns {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-//テスト
+//テストありがとなす
 	}
 
 	private void setup(final FMLCommonSetupEvent event) {
