a06ca74b50a29fe7758f7b6122e1841af48d9f89
kyou0419 <57523862+kyou0419@users.noreply.github.com>
2020-01-18 11:57:07 +0900

    ironplate

--- src/main/java/orz/teamfelnull/mineguns/item/MGItems.java
+++ src/main/java/orz/teamfelnull/mineguns/item/MGItems.java
@@ -15,11 +15,15 @@ public class MGItems {
 
 	public static Item TEST_GUN = new GunItem(new Item.Properties().group(MGItemGroup.MINEGUN_TAB), MGGuns.TEST)
 			.setRegistryName(MineGuns.MODID, "test_gun");
+	
+	public static Item IRON_PLATE = new Item(new Item.Properties().group(MGItemGroup.MINEGUN_TAB))
+			.setRegistryName(MineGuns.MODID, "iron_plate");
 
 	public static void registerItem(IForgeRegistry<Item> r) {
 		//test
 		r.register(MGItems.TEST_ITEM);
 		r.register(MGItems.TEST_GUN);
 		r.register(MGItems.IRON_GEAR);
+		r.register(MGItems.IRON_PLATE);
 	}
 }
